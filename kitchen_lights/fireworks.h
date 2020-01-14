7203e814fedec948cc281f5732be6f5fd9f49755
Jeff Karle <jeff.karle@sapphirehealth.org>
2020-01-14 16:47:22 -0600

    update

--- kitchen_lights/fireworks.h
+++ kitchen_lights/fireworks.h
@@ -1,149 +1,119 @@
 #ifndef FIREWORKS_H
 #define FIREWORKS_H
 
+#define MAX_NUMBER_OF_SPARKS 60
+#define MIN_NUMBER_OF_SPARKS 20
 
+enum stage { FADEUP, EXPLOSION, WAITING };
+uint8_t firework_position;
+uint8_t firework_hue;
+uint8_t firework_bri;
+stage current_stage;
+uint16_t max_wait = 2000;
+uint16_t min_wait = 500;
+uint32_t next_explosion_time;
+bool exploded;
+float gravity = 0.97;
+uint8_t number_of_sparks;
+float spark_pos[MAX_NUMBER_OF_SPARKS];
+float spark_vel[MAX_NUMBER_OF_SPARKS];
+bool spark_dir[MAX_NUMBER_OF_SPARKS];
+float spark_fade[MAX_NUMBER_OF_SPARKS];
+uint8_t firework_bg_fade = 128;
+float spark_bri[MAX_NUMBER_OF_SPARKS];
+uint8_t brightest_spark;
 
+void prepare_for_explosion() {
+	random16_add_entropy(analogRead(2));
+	firework_position = random8(20,NUM_LEDS - 10);
+	current_stage = FADEUP;
+	firework_hue = random8();
+	firework_bri = 0;
+	exploded = false;
+	fill_solid(leds, NUM_LEDS, CRGB::Black);
+}
 
-void fireworks_start(bool old) {
-	if (old) {
-		old_gravs[0].dist_old = 0;
-		old_gravs[0].distance = 0;
-		old_gravs[0].vel_old = streamer_velocity;
-		old_gravs[0].velocity = streamer_velocity;
-		old_gravs[0].fireworks_hue = random8();
-		old_gravs[0].fireworks_sat = 50;
-		old_gravs[0].fireworks_bri = 128;
-		old_fireworks_state = 1;
+void fadeup() {
+	if (exploded) {
+		current_stage = EXPLOSION;
+		number_of_sparks = random8(MIN_NUMBER_OF_SPARKS, MAX_NUMBER_OF_SPARKS + 1);
+		for (uint8_t i = 0; i < number_of_sparks; i++) {
+			spark_pos[i] = (float)firework_position;
+			spark_vel[i] = (float)(random16(10, 125)) / 100;
+			spark_dir[i] = random8(0, 2);
+			spark_fade[i] = random8(5, 15);
+			spark_bri[i] = 255;
+		}
+		leds[firework_position] = CRGB::White;
 	}
 	else {
-		my_gravs[0].dist_old = 0;
-		my_gravs[0].distance = 0;
-		my_gravs[0].vel_old = streamer_velocity;
-		my_gravs[0].velocity = streamer_velocity;
-		my_gravs[0].fireworks_hue = random8();
-		my_gravs[0].fireworks_sat = 50;
-		my_gravs[0].fireworks_bri = 128;
-		fireworks_state = 1;
-	}
-}
-
-void fireworks_up(bool old) {
-	if (old) {
-		old_gravs[0].velocity = old_gravs[0].vel_old + gravity * time_inc;
-		old_gravs[0].distance = old_gravs[0].dist_old + old_gravs[0].velocity * time_inc;
-
-		int i = map(old_gravs[0].distance, 0, 32767, 0, STRIP_LENGTH);
-
-		old_gravs[0].vel_old = old_gravs[0].velocity;
-		old_gravs[0].dist_old = old_gravs[0].distance;
-
-		if (i < STRIP_LENGTH) ringCHSV(1, i, old_gravs[0].fireworks_hue, old_gravs[0].fireworks_sat, old_gravs[0].fireworks_bri);
-		if (old_gravs[0].vel_old <= 0) old_fireworks_state = 2;
-
-		for (int j = 0; j < STRIP_LENGTH; j++) old_leds[j].fadeToBlackBy(old_this_fade);
-
-		if (i > 3) {
-			if (random8() > 220) {
-				ringCHSV(1, i - 3, old_gravs[0].fireworks_hue, random8(100, 200), old_gravs[0].fireworks_bri);
-			}
-		}
-	}
-	else {
-		my_gravs[0].velocity = my_gravs[0].vel_old + gravity * time_inc;
-		my_gravs[0].distance = my_gravs[0].dist_old + my_gravs[0].velocity * time_inc;
-
-		int i = map(my_gravs[0].distance, 0, 32767, 0, STRIP_LENGTH);
-
-		my_gravs[0].vel_old = my_gravs[0].velocity;
-		my_gravs[0].dist_old = my_gravs[0].distance;
-
-		if (i < STRIP_LENGTH) ringCHSV(0, i, my_gravs[0].fireworks_hue, my_gravs[0].fireworks_sat, my_gravs[0].fireworks_bri);
-		if (my_gravs[0].vel_old <= 0) fireworks_state = 2;
-
-		for (int j = 0; j < STRIP_LENGTH; j++) cur_leds[j].fadeToBlackBy(this_fade);
-
-		if (i > 3) {
-			if (random8() > 220) {
-				ringCHSV(0, i - 3, my_gravs[0].fireworks_hue, random8(100, 200), my_gravs[0].fireworks_bri);
-			}
+		firework_bri += 5;
+		leds[firework_position] = CHSV(firework_hue, 255, firework_bri);
+		if (firework_bri >= 255) {
+			exploded = true;
 		}
 	}
 }
 
-void fireworks_explode_init(bool old) {
-	if (old) {
-		for (int k = 1; k < num_gravs; k++) {
-			old_gravs[k].dist_old = old_gravs[0].distance;
-			old_gravs[k].distance = old_gravs[0].distance;
-			old_gravs[k].vel_old = random16(-50, explosion_velocity);
-			old_gravs[k].velocity = old_gravs[k].vel_old;
-			old_gravs[k].fireworks_hue = random8();
-			old_gravs[k].fireworks_sat = 255;
-			old_gravs[k].fireworks_bri = 255;
-		}
-		old_my_count = 0;
-		old_fireworks_state = 3;
+void explosion() {
+	fadeToBlackBy(leds, NUM_LEDS, firework_bg_fade);
+	brightest_spark = 0;
+	for (uint8_t x = 0; x < number_of_sparks; x++) {
+		leds[(int)spark_pos[x]] += CHSV(firework_hue, 255, (uint8_t)spark_bri[x]);
+		if (spark_dir[x]) { spark_pos[x] += spark_vel[x]; }
+		else { spark_pos[x] -= spark_vel[x]; }
+		spark_pos[x] = constrain(spark_pos[x], 0.0, (float)(NUM_LEDS - 1));
+		spark_vel[x] *= gravity;
+		spark_bri[x] *= (1 - (spark_fade[x] / 256));
+		if ((uint8_t)spark_bri[x] > brightest_spark) { brightest_spark = (uint8_t)spark_bri[x]; }
 	}
-	else {
-		for (int k = 1; k < num_gravs; k++) {
-			my_gravs[k].dist_old = my_gravs[0].distance;
-			my_gravs[k].distance = my_gravs[0].distance;
-			my_gravs[k].vel_old = random16(-50, explosion_velocity);
-			my_gravs[k].velocity = my_gravs[k].vel_old;
-			my_gravs[k].fireworks_hue = random8();
-			my_gravs[k].fireworks_sat = 255;
-			my_gravs[k].fireworks_bri = 255;
-		}
-		my_count = 0;
-		fireworks_state = 3;
+	if (brightest_spark <= 5) {
+		fill_solid(leds, NUM_LEDS, CRGB::Black);
+		current_stage = WAITING;
+		next_explosion_time = millis() + random16(min_wait + max_wait);
 	}
 }
 
 
-void fireworks_explode(bool old) {
-	if (old) {
-		if (old_my_count++ >= max_count) old_fireworks_state = 0;
-
-		for (int k = 1; k < num_gravs; k++) {
-			old_gravs[k].velocity = old_gravs[k].vel_old + gravity * time_inc;
-			old_gravs[k].distance = old_gravs[k].dist_old + old_gravs[k].velocity * time_inc;
-
-			int i = map(old_gravs[k].distance, 0, 32767, 0, STRIP_LENGTH);
-			old_gravs[k].vel_old = old_gravs[k].velocity;
-			old_gravs[k].dist_old = old_gravs[k].distance;
-
-			if (old_gravs[k].distance < 0) { old_gravs[k].fireworks_bri = 0; }
-			if (i < STRIP_LENGTH && i >= 0) ringCHSVAdd(1, i, old_gravs[k].fireworks_hue, 255, 255);
+void render_firework() {
+	if (current_stage == WAITING) {
+		if (millis() >= next_explosion_time) {
+			prepare_for_explosion();
 		}
-		for (int j = 0; j < NUM_LEDS; j++) old_leds[j].fadeToBlackBy(old_this_fade);
 	}
-	else {
-		if (my_count++ >= max_count) fireworks_state = 0;
-
-		for (int k = 1; k < num_gravs; k++) {
-			my_gravs[k].velocity = my_gravs[k].vel_old + gravity * time_inc;
-			my_gravs[k].distance = my_gravs[k].dist_old + my_gravs[k].velocity * time_inc;
-
-			int i = map(my_gravs[k].distance, 0, 32767, 0, STRIP_LENGTH);
-			my_gravs[k].vel_old = my_gravs[k].velocity;
-			my_gravs[k].dist_old = my_gravs[k].distance;
-
-			if (my_gravs[k].distance < 0) { my_gravs[k].fireworks_bri = 0; }
-			if (i < STRIP_LENGTH && i >= 0) ringCHSVAdd(0, i, my_gravs[k].fireworks_hue, 255, 255);
-		}
-		for (int j = 0; j < NUM_LEDS; j++) cur_leds[j].fadeToBlackBy(this_fade);
+	else if (current_stage == FADEUP) {
+		fadeup();
+	}
+	else if (current_stage == EXPLOSION) {
+		explosion();
 	}
 }
 
-void fireworks(bool old) {
-  mode_number = 65;
-  switch (fireworks_state) {
-  case 0: fireworks_start(old); break;
-  case 1: fireworks_up(old); break;
-  case 2: fireworks_explode_init(old); break;
-  case 3: fireworks_explode(old); break;
-  default: break;
-  }
+void fireworks() {
+	if (mode_change) {
+		mode_change = 0;
+		use_palette = 0;
+		this_delay = 10;
+		current_stage = WAITING;
+		firework_position = random8(10, NUM_LEDS - 10);
+		firework_hue = random8();
+	}
+	render_firework();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif
