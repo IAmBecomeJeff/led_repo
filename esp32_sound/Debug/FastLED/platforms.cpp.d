ee698b56e62db0e8fc503a24a2376aa39292182c
JeffKarle <jeffrey.karle@gmail.com>
2020-06-22 13:59:36 -0500

    cleaning up stuff

--- esp32_sound/Debug/FastLED/platforms.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\JeffKarle\AppData\Local\Temp\VMBuilds\esp32_sound\esp32_d32\Debug\FastLED\platforms.cpp.o: \
- C:\Users\JeffKarle\Documents\Arduino\libraries\FastLED\platforms.cpp
--- esp32_sound/Debug/VM_DBG/VM_mem_check_sam.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\JeffKarle\AppData\Local\Temp\VMBuilds\esp32_sound\esp32_d32\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
- c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\enterprise\common7\ide\extensions\qgx1xzdm.dlw\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
